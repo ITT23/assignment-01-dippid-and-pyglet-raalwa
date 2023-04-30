6f224c1b14904d9ea7daf2d8ea5d64a77002e388
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-04-30 23:07:01 +0200

    Change to sprite

--- 2d-game/game/obstacle.py
+++ 2d-game/game/obstacle.py
@@ -1,10 +1,12 @@
 import pyglet
 import random
 
+
 class Obstacle():
-    def __init__(self, x_position: int, height: int, color: tuple[int, int, int], width: int, speed: int = 10, y_position: int = 0):
-        self.obstacle = pyglet.shapes.Rectangle(
-            x=x_position, y=y_position, width=width, height=height, color=color)
+    def __init__(self, x_position: int, speed: int = 10, y_position: int = 0):
+        image = pyglet.resource.image('resources/obstacle.png')
+        self.obstacle = pyglet.sprite.Sprite(
+            img=image, x=x_position, y=y_position)
         self.starting_x = x_position
         self.speed = speed
         self.is_game_over = False
@@ -20,7 +22,8 @@ class Obstacle():
         if (self.obstacle.x + self.obstacle.width) < 0:
             self.score += 1
             self.obstacle.x = self.starting_x
-            self.obstacle.height = random.randrange(100,400)
+            # self.obstacle.height = random.randrange(100, 400)
+            self.obstacle.update(scale_y=1+(round(random.random(), 2)*4))
 
     def game_over(self):
         self.is_game_over = True
--- 2d-game/game/player.py
+++ 2d-game/game/player.py
@@ -2,18 +2,15 @@ import pyglet
 
 
 class Player:
-    def __init__(self, x_position: int, radius: int, color: tuple[int, int, int], downward_acceleration: int = -10, y_position: int = 0):
-        image = pyglet.resource.image('resources/background.png')
-        self.avatar = pyglet.sprite.Sprite(img = image,x=x_position, y=y_position)
-        self.starting_y = y_position+radius
-        self.player = pyglet.shapes.Circle(
-            x_position, self.starting_y, radius, color=color)
+    def __init__(self, x_position: int, downward_acceleration: int = -10, y_position: int = 0):
+        image = pyglet.resource.image('resources/player.png')
+        self.player = pyglet.sprite.Sprite(
+            img=image, x=x_position, y=y_position)
         self.is_jumping = False
         self.acceleration = 0
         self.downward_acceleration = downward_acceleration
         self.starting_acceleration = 0
         self.is_game_over = False
-
 
     def update(self):
         if not self.is_game_over:
@@ -31,14 +28,14 @@ class Player:
         if self.acceleration > 0:
             self.acceleration = self.acceleration - 1
         else:
-            if self.player.y > self.starting_y:
+            if self.player.y > 0:
                 self.acceleration = -10
                 #  - 100/(self.player.y - self.starting_y)
                 print(f"Acceleration: {self.acceleration}")
             else:
-                self.player.y = self.starting_y
+                self.player.y = 0
                 self.acceleration = 0
                 self.is_jumping = False
-    
+
     def game_over(self):
         self.is_game_over = True
